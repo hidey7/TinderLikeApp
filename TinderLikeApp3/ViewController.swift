9bff4b08aa5270c9116daaccb8e8808fec611828
hideya <josyuya08@gmail.com>
2021-10-22 19:07:07 +0900

    Initial Commit

---
+++ TinderLikeApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderLikeApp3
+//
+//  Created by 始関秀弥 on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
