3d4e17964c157ce2b5d3946e220ccf4b2650bf52
hideya <josyuya08@gmail.com>
2021-10-23 13:19:30 +0900

    first commit

---
+++ TinderLikeApp3/Extensions/UIView-Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView-Extension.swift
+//  TinderLikeApp3
+//
+//  Created by 始関秀弥 on 2021/10/23.
+//
+
+import Foundation
---
+++ TinderLikeApp3/View/MoveView.swift
@@ -0,0 +1,20 @@
+//
+//  MoveView.swift
+//  TinderLikeApp3
+//
+//  Created by 始関秀弥 on 2021/10/22.
+//
+
+import UIKit
+
+class MoveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
